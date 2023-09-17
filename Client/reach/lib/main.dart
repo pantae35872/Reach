64fc8d4218e196f7b14ceb97954abba0a315194c
Pantae <pantae35872@gmail.com>
2023-09-17 11:34:25 +0700

    Fixing server logic and add preparing encryption

--- Client/reach/lib/main.dart
+++ Client/reach/lib/main.dart
@@ -3,45 +3,31 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:reach/Encryption.dart';
 import 'package:reach/WindowsUI.dart';
-import 'package:cryptography/cryptography.dart';
 
 void main() async {
   final message = utf8.encode("hello world! this is my plaintext.");
 
-  // AES-CBC with 128 bit keys and HMAC-SHA256 authentication.
-  final algorithm = AesCbc.with256bits(
-    macAlgorithm: Hmac.sha256(),
-  );
-  final a = <int>[];
-  for (int i = 0; i < 32; i++) {
-    a.add(0);
-  }
-  final secretKey = await algorithm.newSecretKeyFromBytes(a);
-  final nonce = <int>[];
+  final key = <int>[];
   for (int i = 0; i < 16; i++) {
-    nonce.add(0);
+    key.add(0);
+  }
+  final iv = <int>[];
+  for (int i = 0; i < 16; i++) {
+    iv.add(0);
   }
 
-  // Encrypt
-  final secretBox = await algorithm.encrypt(
-    message,
-    secretKey: secretKey,
-    nonce: nonce
-  );
-  print('Text: ${message}');
-  print('Nonce: ${secretBox.nonce}');
-  print('Ciphertext: ${secretBox.cipherText}');
-  print('MAC: ${secretBox.mac.bytes}');
+  Encryption encryption = Encryption();
+  encryption.set_key(key).set_iv(iv).set_data(message);
 
-  // Decrypt
-  final clearText = await algorithm.decrypt(
-    secretBox,
-    secretKey: secretKey,
-    aad: nonce    
-  );
-  print('Cleartext: ${clearText}');
+  encryption.aes_encrypt();
 
+  print(encryption.build());
+
+  encryption.aes_decrypt();
+
+  print(String.fromCharCodes(encryption.build()));
 
   if (defaultTargetPlatform == TargetPlatform.windows) 
   {
